56cc7aa91f39c6abc4fb2855013b14986cdd517d
coado <dawid.malecki21@gmail.com>
2022-10-24 10:06:07 +0200

    changed github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/coado/snake_speedrun_docs',
         },
         blog: {
           showReadingTime: true,
